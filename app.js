bc2718fcbebf0e6e3cfe8db3fb12fce877998549
rz0585720 <cherish05061231@gmail.com>
2023-06-06 22:01:42 +0800

    add not found page

--- app.js
+++ app.js
@@ -26,11 +26,15 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-	const keyword = req.query.keyword
+	if (!req.query.keyword) {
+		return res.redirect('/')
+	}
+
+	const keyword = req.query.keyword.trim().toLowerCase()
 	const restaurants = results.filter(restaurant => {
 		return (
-			restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-			restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+			restaurant.name.toLowerCase().includes(keyword) ||
+			restaurant.category.toLowerCase().includes(keyword)
 		)
 	})
 	res.render('index', { restaurants: restaurants, keyword: keyword })
