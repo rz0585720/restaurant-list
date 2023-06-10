95ed4fa37cf6fab737e7ada18cf93ea4050bfc1a
rz0585720 <cherish05061231@gmail.com>
2023-06-11 00:32:50 +0800

    refactor mongoose config

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurant')
 const methodOverride = require('method-override')
-const port = 3000
+
 const routes = require('./routes')
 require('./config/mongoose')
 
+const port = 3000
+const app = express()
 // express template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-// setting static files
 app.use(express.static('public'))
-
-// setting body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
