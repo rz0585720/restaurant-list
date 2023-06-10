10740ddc9ea6c22d0635b157ab5f66474f2b3135
rz0585720 <cherish05061231@gmail.com>
2023-06-11 00:47:41 +0800

    fix search function

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,5 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 
 router.use('/', home)
-router.use('/search', home)
 router.use('/restaurants', restaurants)
 module.exports = router
