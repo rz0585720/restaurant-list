9be6551eef680945aa326f8cec8f6b3d5582f994
rz0585720 <cherish05061231@gmail.com>
2023-07-14 21:21:59 +0800

    add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/sort', sort)
+router.use('/users', users)
 module.exports = router
