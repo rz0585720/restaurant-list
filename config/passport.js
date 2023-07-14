773ae31b97d4c7edd2e698253b0233d0b7b8e3a8
rz0585720 <cherish05061231@gmail.com>
2023-07-14 21:47:17 +0800

    add logout feature

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
 
 module.exports = app => { 
   app.use(passport.initialize())
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
@@ -30,6 +31,11 @@ router.post('/register', (req, res) => {
 				.catch(err => console.log(err))
 		}
 	})
+})
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
 })
 
 module.exports = router
