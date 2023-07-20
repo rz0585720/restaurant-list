bf36cc9b32a2cac7459b824490eb83213e47ffa8
rz0585720 <cherish05061231@gmail.com>
2023-07-20 18:08:02 +0800

    use async/await to create seed data

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -3,70 +3,39 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const Restaurant = require('../restaurant')
-const restaurantList = require('../../restaurant.json').results
-const User = require('../user')
+const { users, restaurants } = require('../../data')
+const RestaurantModel = require('../restaurant')
+const UserModel = require('../user')
 const db = require('../../config/mongoose')
 
-const SEED_USER1 = {
-	email: 'user1@example.com',
-	password: '12345678',
-	restaurantsOwned: [0, 1, 2],
-}
+db.once('open', async () => {
+	try {
+		await Promise.all(
+			users.map(async (user, user_index) => {
+				const salt = await bcrypt.genSalt(10)
+				const hash = await bcrypt.hash(user.password, salt)
+				const createdUser = await UserModel.create({
+					name: user.name,
+					email: user.email,
+					password: hash,
+				})
+				console.log('user created')
 
-const SEED_USER2 = {
-	email: 'user2@example.com',
-	password: '12345678',
-	restaurantsOwned: [3, 4, 5],
-}
-
-db.once('open', () => {
-	bcrypt
-		.genSalt(10)
-		.then(salt => bcrypt.hash(SEED_USER1.password, salt))
-		.then(hash =>
-			User.create({
-				email: SEED_USER1.email,
-				password: hash,
+				const userRestaurant = []
+				restaurants.forEach((restaurant, rest_index) => {
+					if (rest_index >= 3 * user_index && rest_index < 3 * (user_index + 1)) {
+						restaurant.userId = createdUser._id
+						userRestaurant.push(restaurant)
+					}
+				})
+				await RestaurantModel.create(userRestaurant)
+				console.log('restaurant created')
 			})
 		)
-		.then(user => {
-			const userId = user._id
-			return Promise.all(
-				Array.from({length: SEED_USER1.restaurantsOwned.length}, (_, i) =>
-					Restaurant.create({
-						...restaurantList[SEED_USER1.restaurantsOwned[i]],
-						userId,
-					})
-				)
-			)
-		})
-		.then(() => {
-			console.log('user1 is done.')
-		})
-
-	bcrypt
-		.genSalt(10)
-		.then(salt => bcrypt.hash(SEED_USER2.password, salt))
-		.then(hash =>
-			User.create({
-				email: SEED_USER2.email,
-				password: hash,
-			})
-		)
-		.then(user => {
-			const userId = user._id
-			return Promise.all(
-				Array.from({length: SEED_USER2.restaurantsOwned.length}, (_, i) =>
-					Restaurant.create({
-						...restaurantList[SEED_USER2.restaurantsOwned[i]],
-						userId,
-					})
-				)
-			)
-		})
-		.then(() => {
-			console.log('user2 is done.')
-			process.exit()
-		})
+		console.log('All users and restaurants are created!')
+		process.exit()
+	}
+	catch (err) {
+		console.log(err)
+	}
 })
